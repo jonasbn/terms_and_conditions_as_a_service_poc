a07f4715a9626c1882eb0c8dc8077be83b240034
jonasbn <jonasbn@gmail.com>
2017-11-02 20:41:50 +0100

    Added names to the routes, these can be listed using: clear; carton exec -- script/tac routes

--- lib/TAC.pm
+++ lib/TAC.pm
@@ -32,37 +32,36 @@ sub startup {
 
     # Route to current terms and conditions, defaulting to danish
     $r->get('/terms_and_conditions')
-        ->to('controller#terms_and_conditions_by_date');
+        ->to('controller#terms_and_conditions_by_date')->name('get current terms and conditions by default language');
 
     # Route to current terms and conditions, with language label
     $r->get('/:language/terms_and_conditions')
-        ->to('controller#terms_and_conditions_by_date');
+        ->to('controller#terms_and_conditions_by_date')->name('get current terms and conditions by language');
 
     # Route to terms and conditions by date, defaulting to danish
     $r->get( '/terms_and_conditions/:date' => [ date => qr/\d{8}/ ] )
-        ->to('controller#terms_and_conditions_by_date');
+        ->to('controller#terms_and_conditions_by_date')->name('get terms and conditions by default language and date');
 
     # Route to terms and conditions by date, with language label
     $r->get(
         '/:language/terms_and_conditions/:date' => [ date => qr/\d{8}/ ] )
-        ->to('controller#terms_and_conditions_by_date');
+        ->to('controller#terms_and_conditions_by_date')->name('get terms and conditions by language and date');
 
     # Route to terms and conditions by revision, defaulting to danish
     $r->get(
         '/terms_and_conditions/revision/:revision' => [ revision => qr/\d+/ ]
-    )->to('controller#terms_and_conditions_by_revision');
+    )->to('controller#terms_and_conditions_by_revision')->name('get terms and conditions by default language and revision');
 
     # Route to terms and conditions by revision, with language label
     $r->get( '/:language/terms_and_conditions/revision/:revision' =>
             [ revision => qr/\d+/ ] )
-        ->to('controller#terms_and_conditions_by_revision');
+        ->to('controller#terms_and_conditions_by_revision')->name('get terms and conditions by language and revision');
 
         # Default route to controller, defaulting to danish
-        $r->get('/')->to('controller#default');
+        $r->get('/')->to('controller#default')->name('get default page');
 
         # Default route with language label
-        $r->get('/:language')->to('controller#default');
-
+        $r->get('/:language')->to('controller#default')->name('get default page by language');
 }
 
 1;
